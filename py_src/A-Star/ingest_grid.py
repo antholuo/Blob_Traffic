27da91c392d8edda397ddcc0a443c7ecc7e4c9bd
Anthony Luo <anthonyluo100@Gmail.com>
2022-01-07 23:04:32 -0500

    Array indexes can't be addressed before initialization. Need sol'n

--- py_src/A-Star/ingest_grid.py
+++ py_src/A-Star/ingest_grid.py
@@ -26,9 +26,58 @@ def yx_to_xy(yx_grid):
     0 d e f          [e, b],
       0 1 2          [f, c]]
     """
-    xy_grid = []
     len_x = len(yx_grid[0]) # any index works, grid should be same length anywhere.
+    len_y = len(yx_grid)    # how many y indices there are.
+    xy_grid = [[]* len_y] * len_x
+    # note that the above may change as we edit our code. I will think of a solution.
 
+    # generate locations for us to follow.
+    x_loc = 0
+    y_loc = len_y -1 # note that the y direction is flipped, so our y_loc actually starts at the TOP.
+
+    for row in yx_grid:
+        x_loc = 0
+        for column in row:
+            print(x_loc, y_loc, column)
+            # iterating through the yx_style grid normally. (column is actually a value)
+            xy_grid[x_loc][y_loc] = column # you can't actually do this...
+            x_loc += 1
+        y_loc -= 1
+        print(y_loc)
+
+    return xy_grid
+
+# simple visualization of the maze and the path A* takes
+def visualize(maze, path=[]):
+    for coordinate in path:
+        maze[coordinate[0]][coordinate[1]] = "#"
+
+    x = 0
+    y = 0
+    max_x = len(maze)
+    max_y = len(maze[1])
+    while y < max_y:
+        x = 0
+        print()
+        while x < max_x:
+            print(maze[x][max_y - y - 1], end=" ")
+            x += 1
+        y += 1
+
+def testfunc1():
+    maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    xy_grid = yx_to_xy(maze)
+    visualize(xy_grid)
 
 if __name__ == "__main__":
     print("running ingest_grid.py")
+    testfunc1()
