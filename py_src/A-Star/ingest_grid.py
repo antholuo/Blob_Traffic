26e9ff4f6b02734a4f3130b1f7c15e488ef6df21
Anthony Luo <anthonyluo100@Gmail.com>
2022-01-07 21:52:03 -0500

    init ingest_grid.py

---
+++ py_src/A-Star/ingest_grid.py
@@ -0,0 +1,9 @@
+"""
+ingest_grid.py, written as part of Blob_Traffic project (https://github.com/antholuo/Blob_Traffic)
+---------------------------------------
+January 2022
+Anthony Luo (antholuo@gmail.com), Christina Zhang (christinaytzhangroxs@gmail.com)
+---------------------------------------
+    This file is designed to take a line-by-line entry (cin) or a text file (preferred) and parse it into a workable
+format for the rest of our code.
+"""
