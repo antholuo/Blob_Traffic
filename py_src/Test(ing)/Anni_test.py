821f28217c87874368e035219a4fae4215ab11e4
Anthony Luo <anthonyluo100@Gmail.com>
2022-01-10 22:21:31 -0500

    non-working cube/ball assembly

--- py_src/Test(ing)/Anni_test.py
+++ py_src/Test(ing)/Anni_test.py
@@ -2,6 +2,7 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
 import sys
+import time
 
 name = 'ball_glut'
 
@@ -11,6 +12,7 @@ def main():
     glutInitWindowSize(400,400)
     glutCreateWindow(name)
 
+
     glClearColor(0.,0.,0.,1.)
     glShadeModel(GL_SMOOTH)
     glEnable(GL_CULL_FACE)
@@ -18,18 +20,30 @@ def main():
     glEnable(GL_LIGHTING)
     lightZeroPosition = [10.,4.,10.,1.]
     lightZeroColor = [0.8,1.0,0.8,1.0] #green tinged
+    lightOnePosition = [1, 12, 1, 1]
+    lightOneColor = [1.0, 0.2, 0.2, 1.0]
     glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
     glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
     glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
     glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT1, GL_POSITION, lightOnePosition)
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, lightOneColor)
+    glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION, 0.1)
+    glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.05)
+    glEnable(GL_LIGHT1)
+
+    rotation()
+
     glutDisplayFunc(display)
     glMatrixMode(GL_PROJECTION)
     gluPerspective(40.,1.,1.,40.)
     glMatrixMode(GL_MODELVIEW)
+
     gluLookAt(0,0,10,
               0,0,0,
               0,1,0)
+
     glPushMatrix()
     glutMainLoop()
     return
@@ -37,11 +51,46 @@ def main():
 def display():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     glPushMatrix()
-    color = [1.0,0.,0.,1.]
+
+    color = [1.0,0.4,0.7,0.7]
     glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
-    glutSolidSphere(2,20,20)
+
+    glutSolidSphere(2,200,200)
+    glutSolidCube(3)
+
     glPopMatrix()
     glutSwapBuffers()
     return
 
-if __name__ == '__main__': main()
+def rotation():
+    glMatrixMode(GL_MODELVIEW)
+    modelMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+    gluLookAt(0, 0, 10,
+              0, 0, 0,
+              0, 1, 0)
+
+    while True:
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        glPushMatrix()
+        glRotated(5, 1, 0, 0)
+
+        glMultMatrixf(modelMatrix)
+        modelMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+
+        glLoadIdentity()
+        glTranslatef(0, 0, -5)
+        glMultMatrixf(modelMatrix)
+        color = [1.0, 0.4, 0.7, 0.7]
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, color)
+
+        glutSolidSphere(2, 200, 200)
+        glutSolidCube(3)
+
+        glPopMatrix()
+        glutSwapBuffers()
+
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
