82b03e0cc11507f7f5d6aba86ff944c04375b540
Anthony Luo <anthonyluo100@Gmail.com>
2022-01-10 21:02:16 -0500

    poc on pyopengl

--- py_src/Test(ing)/Anni_test.py
+++ py_src/Test(ing)/Anni_test.py
@@ -1,13 +1,32 @@
+import glfw
+
+from OpenGL.GL import *
+
 import numpy as np
 
-arr = np.array([['a','b','c'],['d','e','f']])
+glfw.init()
+# creating a window with 800 width and 600 height
+window = glfw.create_window(800,600, "PyOpenGL Init", None, None)
+glfw.set_window_pos(window,400,200)
+glfw.make_context_current(window)
 
-print(arr)
+vertices = [-0.5, -0.5, 0.0,
+0.5, -0.5,0.0,
+0.0, 0.5, 0.0]
+v = np.array(vertices,dtype=np.float32)
 
-print(arr[1,0])
 
-mytuple = (1,0)
+glEnableClientState(GL_VERTEX_ARRAY)
+glVertexPointer(3, GL_FLOAT,0,v)
+# setting color for background
+glClearColor(0,0,0,0)
 
-print(mytuple[1])
+while not glfw.window_should_close(window):
 
-print(arr[1][0])
+    glfw.poll_events()
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glDrawArrays(GL_TRIANGLES,0,3)
+    glfw.swap_buffers(window)
+
+glfw.terminate()
