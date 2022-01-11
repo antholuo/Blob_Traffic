b4ab4d56ad947b9f096432527c2e54d963f2a183
Anthony Luo <anthonyluo100@Gmail.com>
2022-01-10 21:28:31 -0500

    sphere

--- py_src/Test(ing)/Anni_test.py
+++ py_src/Test(ing)/Anni_test.py
@@ -1,32 +1,47 @@
-import sys
-from OpenGL.GL import *
-from OpenGL.GLU import *
 from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+import sys
 
-# The display() method does all the work; it has to call the appropriate
-# OpenGL functions to actually display something.
+name = 'ball_glut'
+
+def main():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(400,400)
+    glutCreateWindow(name)
+
+    glClearColor(0.,0.,0.,1.)
+    glShadeModel(GL_SMOOTH)
+    glEnable(GL_CULL_FACE)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_LIGHTING)
+    lightZeroPosition = [10.,4.,10.,1.]
+    lightZeroColor = [0.8,1.0,0.8,1.0] #green tinged
+    glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
+    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
+    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
+    glEnable(GL_LIGHT0)
+    glutDisplayFunc(display)
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(40.,1.,1.,40.)
+    glMatrixMode(GL_MODELVIEW)
+    gluLookAt(0,0,10,
+              0,0,0,
+              0,1,0)
+    glPushMatrix()
+    glutMainLoop()
+    return
+
 def display():
-    # Clear the color and depth buffers
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    # ... render stuff in here ...
-    # It will go to an off-screen frame buffer.
-
-    # Copy the off-screen buffer to the screen.
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glPushMatrix()
+    color = [1.0,0.,0.,1.]
+    glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
+    glutSolidSphere(2,20,20)
+    glPopMatrix()
     glutSwapBuffers()
+    return
 
-glutInit(sys.argv)
-
-# Create a double-buffer RGBA window.   (Single-buffering is possible.
-# So is creating an index-mode window.)
-glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
-
-# Create a window, setting its title
-glutCreateWindow('interactive')
-
-# Set the display callback.  You can set other callbacks for keyboard and
-# mouse events.
-glutDisplayFunc(display)
-
-# Run the GLUT main loop until the user closes the window.
-glutMainLoop()
+if __name__ == '__main__': main()
